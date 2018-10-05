a4e3e83db98c9f6b253a049a57017e47d9ea363d
saixenia <julian-rodriguez92@hotmail.com>
2018-10-05 17:46:39 -0500

    actualizacion Detalle_cancha

--- app/src/main/java/com/example/julian/myapplication/detalle_cancha.java
+++ app/src/main/java/com/example/julian/myapplication/detalle_cancha.java
@@ -1,13 +1,22 @@
 package com.example.julian.myapplication;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class detalle_cancha extends AppCompatActivity {
-
+    ImageView imageView2;
+    ListView _dynamic_lista;
+    FloatingActionButton add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_cancha);
+        imageView2 = findViewById(R.id.imageView2);
+        _dynamic_lista = findViewById(R.id._dynamic_lista);
+        add = findViewById(R.id.add);
+
     }
 }
